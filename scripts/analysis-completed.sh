9020a6bc505aff280153e9ecc2b6246ee23af83e
Christopher Zell <zelldon91@googlemail.com>
2020-11-13 16:30:02 +0100

    chore(scripts): add hints for analysis completed

--- scripts/analysis-completed.sh
+++ scripts/analysis-completed.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-set -euox pipefail
+set -eo pipefail
 
+if [ -z "$1" ]
+then
+  echo "Please provide as first argument the cluster id for which you finished the analysis (without '-zeebe').
+Example execution $0 'cluster-id'"
+ exit 1
+fi
 
 source credentials
 
