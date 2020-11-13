01b952d961251337714b4f2c0df984c89b063974
Christopher Zell <zelldon91@googlemail.com>
2020-11-13 16:26:59 +0100

    chore(scripts): rework schedule daily test run

---
+++ scripts/scheduleDailyTestRun.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+if [ -z "$1" ]
+then
+ echo "Please provide as first argument the id and second the generation you want to run with.
+Example execution $0 'zell' 'Zeebe 0.24.4'"
+ exit 1
+fi
+
+if [ -z "$2" ]
+then
+ echo "Please provide as first argument the id and second the generation you want to run with.
+Example execution $0 'zell' 'Zeebe 0.24.4'"
+ exit 1
+fi
+
+source credentials
+
+id=$1
+generation=$2
+var=$(jq -n --arg id "$id" \
+      --arg generation "$generation" \
+      '{id: $id, generation: $generation}')
+
+zbctl create instance daily-test-protocol --variables "$var"
