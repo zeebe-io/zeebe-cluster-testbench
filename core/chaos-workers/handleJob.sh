3b000f9fce6354c29c71f29ebb162f650e830a87
Christopher Zell <zelldon91@googlemail.com>
2020-11-17 16:12:26 +0100

    chore(chaos): use runChaos script

--- core/chaos-workers/handleJob.sh
+++ core/chaos-workers/handleJob.sh
@@ -46,18 +46,16 @@ kubectl get pods &>> "$logFile"
 
 cd "zeebe-chaos/chaos-experiments/camunda-cloud/" || exit 1
 
+# add scripts to path
+source runChaos.sh
+
 # deploy workers which are needed in some of our chaos experiments
 # Be aware that we are not delete them here, since if the experiments fails we might want to check
 # the logs of the workers AND they are deleted if we delete the namespace anyway.
 kubectl apply -f worker.yaml &>> "$logFile"
 
-# add scripts to path
-PATH="$PATH:$(pwd)/scripts/"
-export PATH
-
 # Get latest state of the repo
 git pull origin master &>> "$logFile"
-
 
 # We using a glob to get all experiments for a clusterplan, but if no files exist
 # the glob is replaced with itself. To avoid that we use nullglob, which will replace it with null.
@@ -66,7 +64,7 @@ git pull origin master &>> "$logFile"
 # Bash loop over a list of (non-existing) files https://www.endpoint.com/blog/2016/12/12/bash-loop-wildcards-nullglob-failglob
 # Shopts: https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 #
-# This makes the implementation easier, since we get an empty array as parameter, which means we skip the execution and mark the job as skipped. 
+# This makes the implementation easier, since we get an empty array as parameter, which means we skip the execution and mark the job as skipped.
 shopt -s nullglob
 runChaosExperiments chaosRunner "$clusterPlan"/*/experiment.json
 shopt -u nullglob
