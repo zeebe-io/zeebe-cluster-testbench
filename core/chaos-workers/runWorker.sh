60716a7d96673de4682720f18ccb53f9e040b23e
Christopher Zell <zelldon91@googlemail.com>
2020-11-18 09:48:25 +0100

    fix(chaos): remove insecure flag

--- core/chaos-workers/runWorker.sh
+++ core/chaos-workers/runWorker.sh
@@ -7,7 +7,7 @@ zbctl status
 # Registers worker - will run `handleJob.sh` for each new job
 
 
-zbctl --insecure create worker chaos-experiments \
+zbctl create worker chaos-experiments \
       --concurrency 1 \
       --maxJobsActive 1 \
       --timeout $(( 1 * 60 * 60  ))s \
