815a31024fad873f07f3c787b38f4cafb38183d5
pihme <pihme@users.noreply.github.com>
2020-11-25 11:08:13 +0100

    chore(cloud-client): add response filter to get more information for bad requests!

--- cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/oauth/OAuthClientFactory.java
+++ cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/oauth/OAuthClientFactory.java
@@ -1,5 +1,6 @@
 package io.zeebe.clustertestbench.cloud.oauth;
 
+import io.zeebe.clustertestbench.cloud.filter.BadRequestResponseFilter;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -8,7 +9,8 @@ import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 public class OAuthClientFactory {
 
   public OAuthClient createOAuthClient(String authenticationURL) {
-    Client client = ClientBuilder.newClient();
+    Client client = ClientBuilder.newBuilder().register(BadRequestResponseFilter.class).build();
+
     WebTarget target = client.target(authenticationURL);
     ResteasyWebTarget rtarget = (ResteasyWebTarget) target;
     return rtarget.proxy(OAuthClient.class);
