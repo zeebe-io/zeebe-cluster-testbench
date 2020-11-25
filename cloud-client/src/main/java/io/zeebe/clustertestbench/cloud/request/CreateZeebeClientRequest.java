677c8f6601757b77c2775b5aefea4f5d0ffdfc57
pihme <pihme@users.noreply.github.com>
2020-11-25 09:49:26 +0100

    fix(cloud-client): incorporate changes in backend implementation

--- cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/request/CreateZeebeClientRequest.java
+++ cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/request/CreateZeebeClientRequest.java
@@ -1,7 +1,11 @@
 package io.zeebe.clustertestbench.cloud.request;
 
+import java.util.Collections;
+import java.util.List;
+
 public class CreateZeebeClientRequest {
 
+  private static final List<String> PERMISSIONS = Collections.singletonList("zeebe");
   private final String clientName;
 
   public CreateZeebeClientRequest(String clientName) {
@@ -10,6 +14,10 @@ public class CreateZeebeClientRequest {
 
   public String getClientName() {
     return clientName;
+  }
+
+  public List<String> getPermissions() {
+    return PERMISSIONS;
   }
 
   @Override
