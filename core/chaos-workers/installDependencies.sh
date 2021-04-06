b8c284fd10ad36d9d6ed7f26baf72430c65f6805
Christopher Zell <zelldon91@googlemail.com>
2021-04-06 14:29:09 +0200

    chore(chaos): rm unnecessary dependencies
     * chaostoolkit is no longer used
     * kubens is no longer used

--- core/chaos-workers/installDependencies.sh
+++ core/chaos-workers/installDependencies.sh
@@ -6,13 +6,7 @@ set -exuo
 # apk --no-cache add bash make curl openssl diffutils git jq coreutils
 apt-get update && apt-get install -y bash make curl openssl diffutils git jq coreutils
 
-# pip install chaostoolkit
-# chaos --version
-
 kubectl_version=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
 install kubectl /usr/local/bin/
 kubectl version --client
-
-curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
-install kubens /usr/local/bin/
