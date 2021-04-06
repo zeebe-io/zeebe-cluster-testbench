0fb0bbd47627dc28b6285746e50d1a767b083281
Christopher Zell <zelldon91@googlemail.com>
2021-04-06 09:20:22 +0200

    chore(chaos): adjust dockerfile
     Based on the new application the dockerfile need to be adjusted.

--- core/chaos-workers/installDependencies.sh
+++ core/chaos-workers/installDependencies.sh
@@ -3,10 +3,11 @@ set -exuo
 
 # contains all dependencies which needs to be installed for the chaos docker image
 
-apk --no-cache add bash make curl openssl diffutils git jq coreutils
+# apk --no-cache add bash make curl openssl diffutils git jq coreutils
+apt-get update && apt-get install -y bash make curl openssl diffutils git jq coreutils
 
-pip install chaostoolkit
-chaos --version
+# pip install chaostoolkit
+# chaos --version
 
 kubectl_version=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
