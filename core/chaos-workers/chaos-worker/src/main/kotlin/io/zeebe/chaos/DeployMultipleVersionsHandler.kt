b6559de080abb65aa10b729f31f5ac230e438d8b
Nico Korthout <nico.korthout@camunda.com>
2021-06-08 15:58:22 +0200

    fix(chaos-workers): await deploy v1 in multiversion
    It could happen that deployment of the initial version of the
    multiVersion process would fail. The handler awaits deployment of the
    successive versions and in the end expects that 10 different versions
    were deployed, but this could be lower if one of the model_v1
    deployments failed.
    
    We can mitigate this by also awaiting this deployment until successful.

--- core/chaos-workers/chaos-worker/src/main/kotlin/io/zeebe/chaos/DeployMultipleVersionsHandler.kt
+++ core/chaos-workers/chaos-worker/src/main/kotlin/io/zeebe/chaos/DeployMultipleVersionsHandler.kt
@@ -2,19 +2,19 @@ package io.zeebe.chaos;
 
 import io.zeebe.client.ZeebeClient
 import io.zeebe.client.api.response.ActivatedJob
+import io.zeebe.client.api.response.DeploymentEvent
 import io.zeebe.client.api.worker.JobClient
 import io.zeebe.client.api.worker.JobHandler
 import io.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder
 import io.zeebe.model.bpmn.Bpmn
-import io.zeebe.model.bpmn.BpmnModelInstance
-import org.camunda.bpm.model.xml.ModelInstance
+import org.awaitility.kotlin.await
 
 class DeployMultipleVersionsHandler : JobHandler {
 
     private val PROCESS_ID = "multiVersion"
-    private val MODEL_V1 = Bpmn.createExecutableProcess(PROCESS_ID).name("v1").startEvent().endEvent().done()
-    private val MODEL_V2 = Bpmn.createExecutableProcess(PROCESS_ID).name("v2").startEvent().endEvent().done()
-    private val LOG = org.slf4j.LoggerFactory.getLogger("io.zeebe.chaos.DeployMultipleVersionsHandler")
+    private val RESOURCE_NAME = PROCESS_ID +".bpmn"
+    private val LOG =
+        org.slf4j.LoggerFactory.getLogger("io.zeebe.chaos.DeployMultipleVersionsHandler")
 
     companion object {
         const val JOB_TYPE = "deploy-different-versions.sh"
@@ -28,11 +28,10 @@ class DeployMultipleVersionsHandler : JobHandler {
         createClient(authenticationDetails).use {
             LOG.info("Connected to ${it.configuration.gatewayAddress}, start deploying multiple versions...")
 
-            var lastVersion = -1
-            for (i in 1..5) {
-                deployModel(it, MODEL_V1, "modelV1.bpmn")
-                lastVersion = deployModel(it, MODEL_V2, "modelV2.bpmn")
-            }
+            val lastVersion = IntRange(1, 10)
+                    .map{i -> waitForModelDeployment(it, i)}
+                    .map{e -> e?.workflows?.get(0)?.version ?: -1 }
+                    .last()
 
             LOG.info("Deployed 10 different versions of process $PROCESS_ID, last version: $lastVersion. Complete $JOB_TYPE")
         }
@@ -40,21 +39,22 @@ class DeployMultipleVersionsHandler : JobHandler {
         client.newCompleteCommand(job.key).send()
     }
 
-    private fun deployModel(client: ZeebeClient, model: BpmnModelInstance, name: String) : Int{
-        var version = -1
-        do {
-            try {
-                val deploymentEvent = client.newDeployCommand().addWorkflowModel(model, name).send().join()
-                version = deploymentEvent.workflows[0].version
-            } catch (e: Exception) {
-                // try again
-                LOG.debug("Failed to deploy $name, try again.", e)
-                Thread.sleep(100)
-            }
-        } while (version == -1)
-        return version
+    private fun waitForModelDeployment(client: ZeebeClient, index: Int): DeploymentEvent? {
+        var event: DeploymentEvent? = null
+        await.untilAsserted {
+            event = client.newDeployCommand()
+                    .addWorkflowModel(
+                            Bpmn.createExecutableProcess(PROCESS_ID)
+                                    .name("v1")
+                                    .startEvent("start-" + index)
+                                    .endEvent()
+                                    .done(),
+                            RESOURCE_NAME)
+                    .send()
+                    .join()
+        }
+        return event
     }
-
 
     private fun createClient(authenticationDetails: Map<String, Any>): ZeebeClient {
         val clientId = authenticationDetails["clientId"]!!.toString()
