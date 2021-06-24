6368021a7392fb1c38cf301d2368104dd9aababe
Nico Korthout <nico.korthout@camunda.com>
2021-06-24 19:56:07 +0200

    style(cloud-client): reorder ClusterInfo properties
    Use the same order as the ClusterInfo definition used in the actual api.
    This makes comparison with the response as well as with the api easier.

--- cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/response/ClusterInfo.java
+++ cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/response/ClusterInfo.java
@@ -5,34 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ClusterInfo {
 
-  private ClusterPlanTypeInfo planType;
-
-  private K8sContextInfo k8sContext;
-
   private String uuid;
   private String name;
-
+  private ClusterPlanTypeInfo planType;
+  private K8sContextInfo k8sContext;
   private GenerationInfo generation;
-
   private ChannelInfo channel;
-
   private ClusterStatus status;
-
-  public ClusterPlanTypeInfo getPlanType() {
-    return planType;
-  }
-
-  public void setPlanType(final ClusterPlanTypeInfo planType) {
-    this.planType = planType;
-  }
-
-  public K8sContextInfo getK8sContext() {
-    return k8sContext;
-  }
-
-  public void setK8sContext(final K8sContextInfo k8sContext) {
-    this.k8sContext = k8sContext;
-  }
 
   public String getUuid() {
     return uuid;
@@ -48,6 +27,22 @@ public class ClusterInfo {
 
   public void setName(final String name) {
     this.name = name;
+  }
+
+  public ClusterPlanTypeInfo getPlanType() {
+    return planType;
+  }
+
+  public void setPlanType(final ClusterPlanTypeInfo planType) {
+    this.planType = planType;
+  }
+
+  public K8sContextInfo getK8sContext() {
+    return k8sContext;
+  }
+
+  public void setK8sContext(final K8sContextInfo k8sContext) {
+    this.k8sContext = k8sContext;
   }
 
   public GenerationInfo getGeneration() {
@@ -76,14 +71,14 @@ public class ClusterInfo {
 
   @Override
   public String toString() {
-    return "ClusterInfo [planType="
-        + planType
-        + ", k8sContext="
-        + k8sContext
-        + ", uuid="
+    return "ClusterInfo [uuid="
         + uuid
         + ", name="
         + name
+        + ", planType="
+        + planType
+        + ", k8sContext="
+        + k8sContext
         + ", generation="
         + generation
         + ", channel="
@@ -100,16 +95,8 @@ public class ClusterInfo {
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class ClusterPlanTypeInfo {
 
-    private String name;
     private String uuid;
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(final String name) {
-      this.name = name;
-    }
+    private String name;
 
     public String getUuid() {
       return uuid;
@@ -119,13 +106,17 @@ public class ClusterInfo {
       this.uuid = uuid;
     }
 
+    public String getName() {
+      return name;
+    }
+
+    public void setName(final String name) {
+      this.name = name;
+    }
+
     @Override
     public String toString() {
-      return "ClusterPlanTypeInfo [name="
-          + name
-          + ", uuid="
-          + uuid
-          + "]";
+      return "ClusterPlanTypeInfo [uuid=" + uuid + ", name=" + name + "]";
     }
   }
 
@@ -153,27 +144,15 @@ public class ClusterInfo {
 
     @Override
     public String toString() {
-      return "K8sContextInfo [uuid="
-          + uuid
-          + ", name="
-          + name
-          + "]";
+      return "K8sContextInfo [uuid=" + uuid + ", name=" + name + "]";
     }
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class GenerationInfo {
 
-    private String name;
     private String uuid;
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(final String name) {
-      this.name = name;
-    }
+    private String name;
 
     public String getUuid() {
       return uuid;
@@ -183,25 +162,25 @@ public class ClusterInfo {
       this.uuid = uuid;
     }
 
+    public String getName() {
+      return name;
+    }
+
+    public void setName(final String name) {
+      this.name = name;
+    }
+
     @Override
     public String toString() {
-      return "GenerationInfo [name=" + name + ", uuid=" + uuid + "]";
+      return "GenerationInfo [uuid=" + uuid + ", name=" + name + "]";
     }
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class ChannelInfo {
 
-    private String name;
     private String uuid;
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(final String name) {
-      this.name = name;
-    }
+    private String name;
 
     public String getUuid() {
       return uuid;
@@ -211,39 +190,27 @@ public class ClusterInfo {
       this.uuid = uuid;
     }
 
+    public String getName() {
+      return name;
+    }
+
+    public void setName(final String name) {
+      this.name = name;
+    }
+
     @Override
     public String toString() {
-      return "ChannelInfo [name="
-          + name
-          + ", uuid="
-          + uuid
-          + "]";
+      return "ChannelInfo [uuid=" + uuid + ", name=" + name + "]";
     }
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class ClusterStatus {
 
-    private String operateStatus;
-    private String operateUrl; // fixme: has been removed but is still used
     private String ready;
     private String zeebeStatus;
-
-    public String getOperateStatus() {
-      return operateStatus;
-    }
-
-    public void setOperateStatus(final String operateStatus) {
-      this.operateStatus = operateStatus;
-    }
-
-    public String getOperateUrl() {
-      return operateUrl;
-    }
-
-    public void setOperateUrl(final String operateUrl) {
-      this.operateUrl = operateUrl;
-    }
+    private String operateStatus;
+    private String operateUrl; // fixme: has been removed but is still used
 
     public String getReady() {
       return ready;
@@ -261,16 +228,32 @@ public class ClusterInfo {
       this.zeebeStatus = zeebeStatus;
     }
 
+    public String getOperateStatus() {
+      return operateStatus;
+    }
+
+    public void setOperateStatus(final String operateStatus) {
+      this.operateStatus = operateStatus;
+    }
+
+    public String getOperateUrl() {
+      return operateUrl;
+    }
+
+    public void setOperateUrl(final String operateUrl) {
+      this.operateUrl = operateUrl;
+    }
+
     @Override
     public String toString() {
-      return "ClusterStatus [operateStatus="
-          + operateStatus
-          + ", operateUrl="
-          + operateUrl
-          + ", ready="
+      return "ClusterStatus [ready="
           + ready
           + ", zeebeStatus="
           + zeebeStatus
+          + ", operateStatus="
+          + operateStatus
+          + ", operateUrl="
+          + operateUrl
           + "]";
     }
 
@@ -279,5 +262,4 @@ public class ClusterInfo {
      */
 
   }
-
 }
