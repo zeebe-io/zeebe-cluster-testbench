19966f1e796e4d31864da92eb89a5c82ce375b15
Nico Korthout <nico.korthout@camunda.com>
2021-06-24 19:46:20 +0200

    refactor(cloud-client): remove nolonger provided cluster info
    All this schema info is no longer (or was never) provided by
    getClusterInfo or listClusterInfos

--- cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/response/ClusterInfo.java
+++ cloud-client/src/main/java/io/zeebe/clustertestbench/cloud/response/ClusterInfo.java
@@ -1,10 +1,6 @@
 package io.zeebe.clustertestbench.cloud.response;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ClusterInfo {
@@ -14,18 +10,13 @@ public class ClusterInfo {
   private K8sContextInfo k8sContext;
 
   private String uuid;
-  private String ownerId;
   private String name;
-
-  private boolean internal;
 
   private GenerationInfo generation;
 
   private ChannelInfo channel;
 
   private ClusterStatus status;
-
-  private ClusterMetadata metadata;
 
   public ClusterPlanTypeInfo getPlanType() {
     return planType;
@@ -51,28 +42,12 @@ public class ClusterInfo {
     this.uuid = uuid;
   }
 
-  public String getOwnerId() {
-    return ownerId;
-  }
-
-  public void setOwnerId(final String ownerId) {
-    this.ownerId = ownerId;
-  }
-
   public String getName() {
     return name;
   }
 
   public void setName(final String name) {
     this.name = name;
-  }
-
-  public boolean isInternal() {
-    return internal;
-  }
-
-  public void setInternal(final boolean internal) {
-    this.internal = internal;
   }
 
   public GenerationInfo getGeneration() {
@@ -99,14 +74,6 @@ public class ClusterInfo {
     this.status = status;
   }
 
-  public ClusterMetadata getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(final ClusterMetadata metadata) {
-    this.metadata = metadata;
-  }
-
   @Override
   public String toString() {
     return "ClusterInfo [planType="
@@ -115,20 +82,14 @@ public class ClusterInfo {
         + k8sContext
         + ", uuid="
         + uuid
-        + ", ownerId="
-        + ownerId
         + ", name="
         + name
-        + ", internal="
-        + internal
         + ", generation="
         + generation
         + ", channel="
         + channel
         + ", status="
         + status
-        + ", metadata="
-        + metadata
         + "]";
   }
 
@@ -139,26 +100,8 @@ public class ClusterInfo {
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class ClusterPlanTypeInfo {
 
-    private String description;
-    private boolean internal;
     private String name;
     private String uuid;
-
-    public String getDescription() {
-      return description;
-    }
-
-    public void setDescription(final String description) {
-      this.description = description;
-    }
-
-    public boolean isInternal() {
-      return internal;
-    }
-
-    public void setInternal(final boolean internal) {
-      this.internal = internal;
-    }
 
     public String getName() {
       return name;
@@ -178,9 +121,7 @@ public class ClusterInfo {
 
     @Override
     public String toString() {
-      return "ClusterPlanTypeInfo [internal="
-          + internal
-          + ", name="
+      return "ClusterPlanTypeInfo [name="
           + name
           + ", uuid="
           + uuid
@@ -193,8 +134,6 @@ public class ClusterInfo {
 
     private String uuid;
     private String name;
-    private String region;
-    private String zone;
 
     public String getUuid() {
       return uuid;
@@ -212,32 +151,12 @@ public class ClusterInfo {
       this.name = name;
     }
 
-    public String getRegion() {
-      return region;
-    }
-
-    public void setRegion(final String region) {
-      this.region = region;
-    }
-
-    public String getZone() {
-      return zone;
-    }
-
-    public void setZone(final String zone) {
-      this.zone = zone;
-    }
-
     @Override
     public String toString() {
       return "K8sContextInfo [uuid="
           + uuid
           + ", name="
           + name
-          + ", region="
-          + region
-          + ", zone="
-          + zone
           + "]";
     }
   }
@@ -247,7 +166,6 @@ public class ClusterInfo {
 
     private String name;
     private String uuid;
-    private Map<String, String> versions;
 
     public String getName() {
       return name;
@@ -265,57 +183,17 @@ public class ClusterInfo {
       this.uuid = uuid;
     }
 
-    public Map<String, String> getVersions() {
-      return versions;
-    }
-
-    public void setVersions(final Map<String, String> versions) {
-      this.versions = versions;
-    }
-
     @Override
     public String toString() {
-      return "GenerationInfo [name=" + name + ", uuid=" + uuid + ", versions=" + versions + "]";
+      return "GenerationInfo [name=" + name + ", uuid=" + uuid + "]";
     }
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class ChannelInfo {
 
-    private List<GenerationInfo> allowedGenerations = new ArrayList();
-
-    private GenerationInfo defaultGeneration;
-
-    @JsonAlias("isDefault")
-    private boolean isDefault;
-
     private String name;
     private String uuid;
-
-    public List<GenerationInfo> getAllowedGenerations() {
-      return allowedGenerations;
-    }
-
-    public void setAllowedGenerations(final List<GenerationInfo> allowedGenerations) {
-      this.allowedGenerations = allowedGenerations;
-    }
-
-    public GenerationInfo getDefaultGeneration() {
-      return defaultGeneration;
-    }
-
-    public void setDefaultGeneration(GenerationInfo defaultGeneration) {
-      this.defaultGeneration = defaultGeneration;
-    }
-
-    public boolean isDefault() {
-      return isDefault;
-    }
-
-    @JsonAlias("isDefault")
-    public void setDefault(final boolean isDefault) {
-      this.isDefault = isDefault;
-    }
 
     public String getName() {
       return name;
@@ -335,54 +213,11 @@ public class ClusterInfo {
 
     @Override
     public String toString() {
-      return "ChannelInfo [allowedGenerations="
-          + allowedGenerations
-          + ", defaultGenerations="
-          + defaultGeneration
-          + ", isDefault="
-          + isDefault
-          + ", name="
+      return "ChannelInfo [name="
           + name
           + ", uuid="
           + uuid
           + "]";
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class GenerationInfo {
-
-      private String name;
-      private String uuid;
-      private Map<String, String> versions;
-
-      public String getName() {
-        return name;
-      }
-
-      public void setName(final String name) {
-        this.name = name;
-      }
-
-      public String getUuid() {
-        return uuid;
-      }
-
-      public void setUuid(final String uuid) {
-        this.uuid = uuid;
-      }
-
-      public Map<String, String> getVersions() {
-        return versions;
-      }
-
-      public void setVersions(final Map<String, String> versions) {
-        this.versions = versions;
-      }
-
-      @Override
-      public String toString() {
-        return "GenerationInfo [name=" + name + ", uuid=" + uuid + ", versions=" + versions + "]";
-      }
     }
   }
 
@@ -390,10 +225,9 @@ public class ClusterInfo {
   public static class ClusterStatus {
 
     private String operateStatus;
-    private String operateUrl;
+    private String operateUrl; // fixme: has been removed but is still used
     private String ready;
     private String zeebeStatus;
-    private String zeebeUrl;
 
     public String getOperateStatus() {
       return operateStatus;
@@ -427,14 +261,6 @@ public class ClusterInfo {
       this.zeebeStatus = zeebeStatus;
     }
 
-    public String getZeebeUrl() {
-      return zeebeUrl;
-    }
-
-    public void setZeebeUrl(final String zeebeUrl) {
-      this.zeebeUrl = zeebeUrl;
-    }
-
     @Override
     public String toString() {
       return "ClusterStatus [operateStatus="
@@ -445,8 +271,6 @@ public class ClusterInfo {
           + ready
           + ", zeebeStatus="
           + zeebeStatus
-          + ", zeebeUrl="
-          + zeebeUrl
           + "]";
     }
 
@@ -456,79 +280,4 @@ public class ClusterInfo {
 
   }
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  public static class ClusterMetadata {
-
-    private String creationTimestamp;
-    private int generation;
-    private String name;
-    private String resourceVersion;
-    private String selfLink;
-    private String uid;
-
-    public String getCreationTimestamp() {
-      return creationTimestamp;
-    }
-
-    public void setCreationTimestamp(final String creationTimestamp) {
-      this.creationTimestamp = creationTimestamp;
-    }
-
-    public int getGeneration() {
-      return generation;
-    }
-
-    public void setGeneration(final int generation) {
-      this.generation = generation;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(final String name) {
-      this.name = name;
-    }
-
-    public String getResourceVersion() {
-      return resourceVersion;
-    }
-
-    public void setResourceVersion(final String resourceVersion) {
-      this.resourceVersion = resourceVersion;
-    }
-
-    public String getSelfLink() {
-      return selfLink;
-    }
-
-    public void setSelfLink(final String selfLink) {
-      this.selfLink = selfLink;
-    }
-
-    public String getUid() {
-      return uid;
-    }
-
-    public void setUid(final String uid) {
-      this.uid = uid;
-    }
-
-    @Override
-    public String toString() {
-      return "ClusterMetadata [creationTimestamp="
-          + creationTimestamp
-          + ", generation="
-          + generation
-          + ", name="
-          + name
-          + ", resourceVersion="
-          + resourceVersion
-          + ", selfLink="
-          + selfLink
-          + ", uid="
-          + uid
-          + "]";
-    }
-  }
 }
