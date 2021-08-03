782188eaf21bc1ebcfb4f4530820e50b25a55f6f
pihme <pihme@users.noreply.github.com>
2021-08-03 10:12:11 +0200

    Revert "test: remove TriggerMessageStartEventHandlerIT"
    This reverts commit cc0ccd91c5373b17874981f69edb118a5bd1fedd.

---
+++ integration-tests/src/test/java/io/zeebe/clustertestbench/it/handler/TriggerMessageStartEventHandlerIT.java
@@ -0,0 +1,114 @@
+package io.zeebe.clustertestbench.it.handler;
+
+import static java.util.stream.Collectors.toMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import io.zeebe.bpmnspec.runner.zeebe.ZeebeEnvironment;
+import io.zeebe.bpmnspec.runner.zeebe.zeeqs.ZeeqsClient;
+import io.zeebe.bpmnspec.runner.zeebe.zeeqs.ZeeqsClient.ElementInstanceDto;
+import io.zeebe.bpmnspec.runner.zeebe.zeeqs.ZeeqsClient.VariableDto;
+import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.api.response.ProcessInstanceEvent;
+import io.zeebe.client.api.worker.JobWorker;
+import io.zeebe.clustertestbench.handler.TriggerMessageStartEventHandler;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+// todo: update ZeeQS version
+@Disabled("This test is blocked by a incompatible ZeeQS version")
+public class TriggerMessageStartEventHandlerIT {
+
+  ZeebeEnvironment zeebeEnvironment = new ZeebeEnvironment();
+  ZeebeClient zeebeClient;
+  ZeeqsClient zeeqsClient;
+  JobWorker workerRegistration;
+
+  @BeforeEach
+  void setUp() {
+    zeebeEnvironment.setup();
+
+    zeebeClient = zeebeEnvironment.getZeebeClient();
+    zeeqsClient = zeebeEnvironment.getZeeqsClient();
+
+    workerRegistration =
+        zeebeClient
+            .newWorker()
+            .jobType("trigger-message-start-event-job")
+            .handler(new TriggerMessageStartEventHandler(zeebeClient))
+            .timeout(Duration.ofSeconds(10))
+            .open();
+  }
+
+  @AfterEach
+  void tearDown() {
+    workerRegistration.close();
+    zeebeEnvironment.cleanUp();
+  }
+
+  /**
+   * This integration test consists of two processes:
+   *
+   * <ul>
+   *   <lI>Main - this process contains a worker to start the secondary process via message start
+   *       event
+   *   <li>Secondary - this process shall be triggered by the main process
+   * </ul>
+   */
+  @Test
+  void shoulTriggerSecondaryProcess() {
+    // given
+    zeebeClient
+        .newDeployCommand()
+        .addResourceFromClasspath("it/triggermessagestartevent/main.bpmn")
+        .addResourceFromClasspath("it/triggermessagestartevent/secondary.bpmn")
+        .send()
+        .join();
+
+    final var variables = Map.of("key1", "value1", "key2", "value2");
+
+    // when
+    final ProcessInstanceEvent mainProcessCreationResponse =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("main")
+            .latestVersion()
+            .variables(variables)
+            .send()
+            .join();
+
+    // then
+    await()
+        .atMost(1, TimeUnit.MINUTES)
+        .until(() -> zeeqsClient.getWorkflowInstanceKeys().size() == 2);
+
+    final List<Long> processInstanceKeys = zeeqsClient.getWorkflowInstanceKeys();
+
+    processInstanceKeys.remove(mainProcessCreationResponse.getProcessInstanceKey());
+
+    final Long secondaryProcessInstanceKey = processInstanceKeys.get(0);
+
+    final ElementInstanceDto elementDto =
+        zeeqsClient.getElementInstances(secondaryProcessInstanceKey).get(0);
+
+    assertThat(elementDto.getElementId()).isEqualTo("secondary");
+
+    final List<VariableDto> variablesInSecondaryProcess =
+        zeeqsClient.getWorkflowInstanceVariables(secondaryProcessInstanceKey);
+
+    final Map<String, String> variablesInSecondaryProcessAsMap =
+        variablesInSecondaryProcess.stream()
+            .collect(
+                toMap(
+                    dto -> dto.getName(),
+                    dto -> dto.getValue().replace("\"", ""))); // value is wrapped in " characters
+
+    assertThat(variablesInSecondaryProcessAsMap).containsExactlyInAnyOrderEntriesOf(variables);
+  }
+}
