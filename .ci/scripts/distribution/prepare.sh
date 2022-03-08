6cf536748de9469a19cce0df560d4f45081d8413
Ole Schönburg <ole.schoenburg@gmail.com>
2022-03-08 15:55:15 +0100

    build: upgrade to Java 17

--- .ci/scripts/distribution/prepare.sh
+++ .ci/scripts/distribution/prepare.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -eux
 
 apt-get -qq update
-apt-get install --no-install-recommends -qq -y jq libatomic1 bats git wget
-
-# install shell check
-shellcheckVersion=v0.7.1
-wget "https://github.com/koalaman/shellcheck/releases/download/$shellcheckVersion/shellcheck-$shellcheckVersion.linux.x86_64.tar.xz"
-tar -xvf "shellcheck-$shellcheckVersion.linux.x86_64.tar.xz"
-cp "shellcheck-$shellcheckVersion"/shellcheck /usr/bin/shellcheck
+apt-get install --no-install-recommends -qq -y jq libatomic1 bats git wget shellcheck
 
 # install bats
 git clone https://github.com/bats-core/bats-core.git
