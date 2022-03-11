6c578b569ccd1c223d2bbdf2f322a71c8d3b380b
Nico Korthout <nico.korthout@camunda.com>
2022-03-11 18:39:33 +0100

    deps: remove unnecessary zeebe deps
    We were dependend on zeebe-test-util and zeebe-util for completely
    unnecessary parts of code, that could be very easily removed.

---
+++ testdriver/api/src/test/java/io/zeebe/clustertestbench/testdriver/api/JsonAssertions.java
@@ -0,0 +1,20 @@
+package io.zeebe.clustertestbench.testdriver.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+
+public class JsonAssertions {
+
+  public static void assertJsonEquality(final String actual, final String expected) {
+    final var objectMapper = new ObjectMapper();
+    try {
+      Assertions.assertThat(objectMapper.readTree(actual))
+          .isEqualTo(objectMapper.readTree(expected));
+    } catch (final JsonProcessingException e) {
+      Assertions.fail(
+          "Expected valid JSON, but 'actual' was '%s' and 'expected' was '%s'"
+              .formatted(actual, expected));
+    }
+  }
+}
