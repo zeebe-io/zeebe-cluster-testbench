12dee3c2ed3387e889bf65812e797cfb552a8550
Christopher Zell <zelldon91@googlemail.com>
2023-09-26 11:46:53 +0200

    refactor: migrate to new external api client
    Replaces usage of internal client with external api client.
    
    Remove several code and test cases which no longer apply.

--- core/src/main/java/io/zeebe/clustertestbench/handler/CreateGenerationInCamundaCloudHandler.java
+++ core/src/main/java/io/zeebe/clustertestbench/handler/CreateGenerationInCamundaCloudHandler.java
@@ -4,116 +4,46 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.api.worker.JobHandler;
 import io.vavr.control.Either;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.ChannelInfo;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.CreateGenerationRequest;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.GenerationInfo;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.UpdateChannelRequest;
+import io.zeebe.clustertestbench.internal.cloud.ExternalConsoleAPIClient;
+import io.zeebe.clustertestbench.internal.cloud.ExternalConsoleAPIClient.CloneGenerationRequest;
+import io.zeebe.clustertestbench.internal.cloud.ExternalConsoleAPIClient.GenerationInfo;
 import io.zeebe.clustertestbench.util.StringLookup;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class CreateGenerationInCamundaCloudHandler implements JobHandler {
+  private final ExternalConsoleAPIClient consoleApiClient;
 
-  private static final String KEY_ZEEBE_IMAGE = "zeebe";
-  private static final String KEY_OPERATE_IMAGE = "operate";
-  private static final String KEY_OPTIMIZE_IMAGE = "optimize";
-  private static final String KEY_TASKLIST_IMAGE = "tasklist";
-  private static final String KEY_ELASTIC_IMAGE = "elasticSearchOss";
-
-  private final InternalCloudAPIClient internalApiClient;
-
-  public CreateGenerationInCamundaCloudHandler(final InternalCloudAPIClient internalApiClient) {
-    this.internalApiClient = internalApiClient;
+  public CreateGenerationInCamundaCloudHandler(final ExternalConsoleAPIClient consoleApiClient) {
+    this.consoleApiClient = consoleApiClient;
   }
 
   @Override
   public void handle(final JobClient client, final ActivatedJob job) throws Exception {
     final var input = job.getVariablesAsType(Input.class);
 
-    final var generation = createGenerationName();
+    final var generationName = createGenerationName();
     final var templateGeneration = lookupTemplate(input.getGenerationTemplate());
-    final var channelInfo = lookupChannel(input.getChannel());
-
-    final var versionsUnderTest = determineVersionsUnderTest(input);
-    final var generationUUID = createGeneration(generation, templateGeneration, versionsUnderTest);
-
-    try {
-      addGenerationToChannel(channelInfo, generationUUID);
-
-      final var output = new Output(generation, generationUUID);
-
-      client.newCompleteCommand(job.getKey()).variables(output).send().join();
-    } catch (final Exception e) {
-      internalApiClient.deleteGeneration(generationUUID);
-      throw e;
-    }
-  }
-
-  private Map<String, String> determineVersionsUnderTest(final Input input) {
-    final var versions = new HashMap<String, String>();
 
     final var zeebeImage = input.getZeebeImage();
-    Optional.ofNullable(zeebeImage).ifPresent(v -> versions.put(KEY_ZEEBE_IMAGE, v));
-
     final var operateImage = input.getOperateImage();
-    Optional.ofNullable(operateImage).ifPresent(v -> versions.put(KEY_OPERATE_IMAGE, v));
 
-    final var optimizeImage = input.getOptimizeImage();
-    Optional.ofNullable(optimizeImage).ifPresent(v -> versions.put(KEY_OPTIMIZE_IMAGE, v));
+    final var newGenerationInfo =
+        cloneGeneration(templateGeneration.uuid(), generationName, zeebeImage, operateImage);
+    final var output = new Output(generationName, newGenerationInfo.uuid());
 
-    final var tasklistImage = input.getTasklistImage();
-    Optional.ofNullable(tasklistImage).ifPresent(v -> versions.put(KEY_TASKLIST_IMAGE, v));
-
-    final var elasticImage = input.getElasticImage();
-    Optional.ofNullable(elasticImage).ifPresent(v -> versions.put(KEY_ELASTIC_IMAGE, v));
-
-    return versions;
+    client.newCompleteCommand(job.getKey()).variables(output).send().join();
   }
 
-  private String createGeneration(
-      final String generation,
-      final GenerationInfo template,
-      final Map<String, String> versionsUnderTest) {
-    final var versions = new HashMap<>(template.versions());
-    versions.putAll(versionsUnderTest);
-
-    // old generations may contain a "worker" property
-    // which is not accepted anymore with newer versions
-    // of console
-    versions.remove("worker");
+  private GenerationInfo cloneGeneration(
+      final String uuid,
+      final String generationName,
+      final String zeebeImage,
+      final String operateImage) {
 
     final var createGenerationRequest =
-        new CreateGenerationRequest(generation, versions, Collections.emptyList());
+        new CloneGenerationRequest(generationName, zeebeImage, operateImage);
 
-    internalApiClient.createGeneration(createGenerationRequest);
-
-    return findGenerationInfoByName(generation)
-        .getOrElseThrow(
-            msg -> new RuntimeException("Creation of generation unsuccessful: " + generation))
-        .uuid();
-  }
-
-  private void addGenerationToChannel(final ChannelInfo channelInfo, final String generationUUID) {
-    final List<String> allowedGenerationIds =
-        channelInfo.allowedGenerations().stream()
-            .map(GenerationInfo::uuid)
-            .collect(Collectors.toList());
-    allowedGenerationIds.add(generationUUID);
-
-    final var updateChannelRequest =
-        new UpdateChannelRequest(
-            channelInfo.name(),
-            channelInfo.isDefault(),
-            channelInfo.defaultGeneration().uuid(),
-            allowedGenerationIds);
-
-    internalApiClient.updateChannel(channelInfo.uuid(), updateChannelRequest);
+    return consoleApiClient.cloneGeneration(uuid, createGenerationRequest);
   }
 
   protected static String createGenerationName() {
@@ -127,21 +57,12 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
   }
 
   private Either<String, GenerationInfo> findGenerationInfoByName(final String name) {
-    final var generationInfos = internalApiClient.listGenerationInfos();
+    final var generationInfos = consoleApiClient.listGenerationInfos();
 
     final var generationLookup =
         new StringLookup<>("generation", name, generationInfos, GenerationInfo::name, true);
 
     return generationLookup.lookup();
-  }
-
-  private ChannelInfo lookupChannel(final String channel) {
-    final var channelInfos = internalApiClient.listChannelInfos();
-
-    final var channelLookup =
-        new StringLookup<>("channel", channel, channelInfos, ChannelInfo::name, true);
-
-    return channelLookup.lookup().getOrElseThrow(msg -> new IllegalArgumentException(msg));
   }
 
   protected static final class Input {
@@ -174,7 +95,7 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
       return operateImage;
     }
 
-    public void setOperateImage(String operateImage) {
+    public void setOperateImage(final String operateImage) {
       this.operateImage = operateImage;
     }
 
@@ -182,7 +103,7 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
       return optimizeImage;
     }
 
-    public void setOptimizeImage(String optimizeImage) {
+    public void setOptimizeImage(final String optimizeImage) {
       this.optimizeImage = optimizeImage;
     }
 
@@ -190,7 +111,7 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
       return tasklistImage;
     }
 
-    public void setTasklistImage(String tasklistImage) {
+    public void setTasklistImage(final String tasklistImage) {
       this.tasklistImage = tasklistImage;
     }
 
@@ -198,7 +119,7 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
       return elasticImage;
     }
 
-    public void setElasticImage(String elasticImage) {
+    public void setElasticImage(final String elasticImage) {
       this.elasticImage = elasticImage;
     }
 
@@ -230,22 +151,7 @@ public class CreateGenerationInCamundaCloudHandler implements JobHandler {
     }
   }
 
-  protected static final class Output {
-    private final String generation;
-    private final String generationUUID;
-
-    public Output(final String generation, final String generationUUID) {
-      this.generation = generation;
-      this.generationUUID = generationUUID;
-    }
-
-    public String getGeneration() {
-      return generation;
-    }
-
-    public String getGenerationUUID() {
-      return generationUUID;
-    }
+  protected record Output(String generation, String generationUUID) {
 
     @Override
     public String toString() {
--- core/src/test/java/io/zeebe/clustertestbench/handler/CreateGenerationInCamundaCloudHandlerTest.java
+++ core/src/test/java/io/zeebe/clustertestbench/handler/CreateGenerationInCamundaCloudHandlerTest.java
@@ -1,36 +1,18 @@
 package io.zeebe.clustertestbench.handler;
 
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_CHANNEL_NAME;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_CHANNEL_UUID;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_ELASTIC_CURATOR_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_ELASTIC_OSS_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_GENERATION_NAME;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_OPERATE_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_OPTIMIZE_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_TASKLIST_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.DEFAULT_ZEEBE_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_ELASTIC_CURATOR_IMAGEE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_ELASTIC_OSS_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_OPERATE_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_OPTIMIZE_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_TASKLIST_IMAGE;
-import static io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient.KEY_ZEEBE_IMAGE;
+import static io.zeebe.clustertestbench.internal.cloud.StubExternalConsoleAPIClient.DEFAULT_GENERATION_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.entry;
 import static org.camunda.community.zeebe.testutils.ZeebeWorkerAssertions.assertThat;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.CreateGenerationRequest;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.GenerationInfo;
-import io.zeebe.clustertestbench.internal.cloud.InternalCloudAPIClient.UpdateChannelRequest;
-import io.zeebe.clustertestbench.internal.cloud.StubInternalCloudAPIClient;
+import io.zeebe.clustertestbench.internal.cloud.ExternalConsoleAPIClient;
+import io.zeebe.clustertestbench.internal.cloud.ExternalConsoleAPIClient.CloneGenerationRequest;
+import io.zeebe.clustertestbench.internal.cloud.StubExternalConsoleAPIClient;
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.camunda.community.zeebe.testutils.stubs.ActivatedJobStub;
 import org.camunda.community.zeebe.testutils.stubs.JobClientStub;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,16 +21,12 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class CreateGenerationInCamundaCloudHandlerTest {
   private static final String ZEEBE_IMAGE = "zeebeTestImage";
   private static final String OPERATE_IMAGE = "operateTestImage";
-  private static final String OPTIMIZE_IMAGE = "optimizeTestImage";
-  private static final String TASKLIST_IMAGE = "tasklistTestImage";
-  private static final String ELASTIC_IMAGE = "elasticTestImage";
 
   @Nested
   @DisplayName("Handle Job")
@@ -56,15 +34,12 @@ class CreateGenerationInCamundaCloudHandlerTest {
 
     public static final String FIELD_ZEEBE_IMAGE = "zeebeImage";
     public static final String FIELD_OPERATE_IMAGE = "operateImage";
-    public static final String FIELD_OPTIMIZE_IMAGE = "optimizeImage";
-    public static final String FIELD_TASKLIST_IMAGE = "tasklistImage";
-    public static final String FIELD_ELASTIC_IMAGE = "elasticImage";
 
     public static final String FIELD_GENERATION_TEMPLATE = "generationTemplate";
-    public static final String FIELD_CHANNEL = "channel";
-    final InternalCloudAPIClient stubInternalApiClient = new StubInternalCloudAPIClient(false);
+    final ExternalConsoleAPIClient stubExternalConsoleAPIClient =
+        new StubExternalConsoleAPIClient(false);
 
-    InternalCloudAPIClient spyInternalApiClient;
+    ExternalConsoleAPIClient spyInternalApiClient;
 
     CreateGenerationInCamundaCloudHandler sutCreateGenerationHandler;
 
@@ -74,19 +49,14 @@ class CreateGenerationInCamundaCloudHandlerTest {
 
     @BeforeEach
     public void setUp() {
-      spyInternalApiClient = spy(stubInternalApiClient);
+      spyInternalApiClient = spy(stubExternalConsoleAPIClient);
       sutCreateGenerationHandler = new CreateGenerationInCamundaCloudHandler(spyInternalApiClient);
 
       activatedJobStub = jobClientStub.createActivatedJob();
 
       activatedJobStub.setInputVariables(
           Map.of(
-              FIELD_ZEEBE_IMAGE,
-              ZEEBE_IMAGE,
-              FIELD_GENERATION_TEMPLATE,
-              DEFAULT_GENERATION_NAME,
-              FIELD_CHANNEL,
-              DEFAULT_CHANNEL_NAME));
+              FIELD_ZEEBE_IMAGE, ZEEBE_IMAGE, FIELD_GENERATION_TEMPLATE, DEFAULT_GENERATION_NAME));
     }
 
     @Test
@@ -105,48 +75,23 @@ class CreateGenerationInCamundaCloudHandlerTest {
     }
 
     @Test
-    public void shouldThrowRuntimeExceptionIfChannelCannotBeFound() {
-      // given
-      activatedJobStub.setInputVariables(
-          Map.of(
-              FIELD_GENERATION_TEMPLATE,
-              DEFAULT_GENERATION_NAME,
-              FIELD_CHANNEL,
-              "unknown-channel"));
-
-      // when + then
-      assertThatThrownBy(() -> sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub))
-          .isExactlyInstanceOf(IllegalArgumentException.class)
-          .hasMessageContaining("Unable to find channel")
-          .hasMessageContaining("unknown-channel");
-    }
-
-    @Test
     public void shouldCreateGenerationWithDefaultVersions() throws Exception {
       // given
       activatedJobStub.setInputVariables(
-          Map.of(
-              FIELD_GENERATION_TEMPLATE,
-              DEFAULT_GENERATION_NAME,
-              FIELD_CHANNEL,
-              DEFAULT_CHANNEL_NAME));
+          Map.of(FIELD_GENERATION_TEMPLATE, DEFAULT_GENERATION_NAME));
 
       // when
       sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
 
       // then
-      final var argumentCapture = ArgumentCaptor.forClass(CreateGenerationRequest.class);
-      verify(spyInternalApiClient).createGeneration(argumentCapture.capture());
+      final var argumentCapture = ArgumentCaptor.forClass(CloneGenerationRequest.class);
+      final var stringCapture = ArgumentCaptor.forClass(String.class);
+      verify(spyInternalApiClient)
+          .cloneGeneration(stringCapture.capture(), argumentCapture.capture());
       final var request = argumentCapture.getValue();
 
-      assertThat(request.versions())
-          .containsOnly(
-              entry(KEY_ZEEBE_IMAGE, DEFAULT_ZEEBE_IMAGE),
-              entry(KEY_OPERATE_IMAGE, DEFAULT_OPERATE_IMAGE),
-              entry(KEY_OPTIMIZE_IMAGE, DEFAULT_OPTIMIZE_IMAGE),
-              entry(KEY_TASKLIST_IMAGE, DEFAULT_TASKLIST_IMAGE),
-              entry(KEY_ELASTIC_CURATOR_IMAGEE, DEFAULT_ELASTIC_CURATOR_IMAGE),
-              entry(KEY_ELASTIC_OSS_IMAGE, DEFAULT_ELASTIC_OSS_IMAGE));
+      assertThat(request.operateVersion()).isNull();
+      assertThat(request.zeebeVersion()).isNull();
     }
 
     @Test
@@ -155,18 +100,14 @@ class CreateGenerationInCamundaCloudHandlerTest {
       sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
 
       // then
-      final var argumentCapture = ArgumentCaptor.forClass(CreateGenerationRequest.class);
-      verify(spyInternalApiClient).createGeneration(argumentCapture.capture());
+      final var argumentCapture = ArgumentCaptor.forClass(CloneGenerationRequest.class);
+      final var stringCapture = ArgumentCaptor.forClass(String.class);
+      verify(spyInternalApiClient)
+          .cloneGeneration(stringCapture.capture(), argumentCapture.capture());
       final var request = argumentCapture.getValue();
 
-      assertThat(request.versions())
-          .containsOnly(
-              entry(KEY_ZEEBE_IMAGE, ZEEBE_IMAGE),
-              entry(KEY_OPERATE_IMAGE, DEFAULT_OPERATE_IMAGE),
-              entry(KEY_OPTIMIZE_IMAGE, DEFAULT_OPTIMIZE_IMAGE),
-              entry(KEY_TASKLIST_IMAGE, DEFAULT_TASKLIST_IMAGE),
-              entry(KEY_ELASTIC_CURATOR_IMAGEE, DEFAULT_ELASTIC_CURATOR_IMAGE),
-              entry(KEY_ELASTIC_OSS_IMAGE, DEFAULT_ELASTIC_OSS_IMAGE));
+      assertThat(request.operateVersion()).isNull();
+      assertThat(request.zeebeVersion()).isEqualTo(ZEEBE_IMAGE);
     }
 
     @Test
@@ -176,67 +117,27 @@ class CreateGenerationInCamundaCloudHandlerTest {
           Map.of(
               FIELD_OPERATE_IMAGE,
               OPERATE_IMAGE,
-              FIELD_OPTIMIZE_IMAGE,
-              OPTIMIZE_IMAGE,
-              FIELD_TASKLIST_IMAGE,
-              TASKLIST_IMAGE,
               FIELD_GENERATION_TEMPLATE,
-              DEFAULT_GENERATION_NAME,
-              FIELD_CHANNEL,
-              DEFAULT_CHANNEL_NAME));
+              DEFAULT_GENERATION_NAME));
 
       // when
       sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
 
       // then
-      final var argumentCapture = ArgumentCaptor.forClass(CreateGenerationRequest.class);
-      verify(spyInternalApiClient).createGeneration(argumentCapture.capture());
+      final var argumentCapture = ArgumentCaptor.forClass(CloneGenerationRequest.class);
+      final var stringCapture = ArgumentCaptor.forClass(String.class);
+      verify(spyInternalApiClient)
+          .cloneGeneration(stringCapture.capture(), argumentCapture.capture());
       final var request = argumentCapture.getValue();
 
-      assertThat(request.versions())
-          .containsOnly(
-              entry(KEY_ZEEBE_IMAGE, DEFAULT_ZEEBE_IMAGE),
-              entry(KEY_OPERATE_IMAGE, OPERATE_IMAGE),
-              entry(KEY_OPTIMIZE_IMAGE, OPTIMIZE_IMAGE),
-              entry(KEY_TASKLIST_IMAGE, TASKLIST_IMAGE),
-              entry(KEY_ELASTIC_CURATOR_IMAGEE, DEFAULT_ELASTIC_CURATOR_IMAGE),
-              entry(KEY_ELASTIC_OSS_IMAGE, DEFAULT_ELASTIC_OSS_IMAGE));
-    }
-
-    @Test
-    public void shouldCreateGenerationWithSpecificElasticVersion() throws Exception {
-      // given
-      activatedJobStub.setInputVariables(
-          Map.of(
-              FIELD_ELASTIC_IMAGE,
-              ELASTIC_IMAGE,
-              FIELD_GENERATION_TEMPLATE,
-              DEFAULT_GENERATION_NAME,
-              FIELD_CHANNEL,
-              DEFAULT_CHANNEL_NAME));
-
-      // when
-      sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
-
-      // then
-      final var argumentCapture = ArgumentCaptor.forClass(CreateGenerationRequest.class);
-      verify(spyInternalApiClient).createGeneration(argumentCapture.capture());
-      final var request = argumentCapture.getValue();
-
-      assertThat(request.versions())
-          .containsOnly(
-              entry(KEY_ZEEBE_IMAGE, DEFAULT_ZEEBE_IMAGE),
-              entry(KEY_OPERATE_IMAGE, DEFAULT_OPERATE_IMAGE),
-              entry(KEY_OPTIMIZE_IMAGE, DEFAULT_OPTIMIZE_IMAGE),
-              entry(KEY_TASKLIST_IMAGE, DEFAULT_TASKLIST_IMAGE),
-              entry(KEY_ELASTIC_CURATOR_IMAGEE, DEFAULT_ELASTIC_CURATOR_IMAGE),
-              entry(KEY_ELASTIC_OSS_IMAGE, ELASTIC_IMAGE));
+      assertThat(request.operateVersion()).isEqualTo(OPERATE_IMAGE);
+      assertThat(request.zeebeVersion()).isNull();
     }
 
     @Test
     public void shouldThrowExceptionIfGenerationWasNotCreated() {
       // given
-      final var brokenStubInternalApiClient = new StubInternalCloudAPIClient(true);
+      final var brokenStubInternalApiClient = new StubExternalConsoleAPIClient(true);
       final var spyStubInternalApiClient = spy(brokenStubInternalApiClient);
 
       final var sutLocal = new CreateGenerationInCamundaCloudHandler(spyStubInternalApiClient);
@@ -248,29 +149,10 @@ class CreateGenerationInCamundaCloudHandlerTest {
     }
 
     @Test
-    public void shouldAddGenerationToChannel() throws Exception {
-      // when
-      sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
-
-      // then
-      final var argumentCapture = ArgumentCaptor.forClass(UpdateChannelRequest.class);
-      verify(spyInternalApiClient)
-          .updateChannel(Mockito.eq(DEFAULT_CHANNEL_UUID), argumentCapture.capture());
-      final var request = argumentCapture.getValue();
-
-      final var generationUUIDs =
-          stubInternalApiClient.listGenerationInfos().stream()
-              .map(GenerationInfo::uuid)
-              .collect(Collectors.toList());
-
-      assertThat(request.name()).isEqualTo(StubInternalCloudAPIClient.DEFAULT_CHANNEL_NAME);
-      assertThat(request.allowedGenerationIds()).containsExactlyElementsOf(generationUUIDs);
-    }
-
-    @Test
     void shouldSetGenerationNameAndUuidUponCompletion() throws Exception {
       // given
-      final var initialGenerations = new ArrayList<>(stubInternalApiClient.listGenerationInfos());
+      final var initialGenerations =
+          new ArrayList<>(stubExternalConsoleAPIClient.listGenerationInfos());
 
       // when
       sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub);
@@ -278,7 +160,8 @@ class CreateGenerationInCamundaCloudHandlerTest {
       // then
       assertThat(activatedJobStub).completed();
 
-      final var createdGenerations = new ArrayList<>(stubInternalApiClient.listGenerationInfos());
+      final var createdGenerations =
+          new ArrayList<>(stubExternalConsoleAPIClient.listGenerationInfos());
       createdGenerations.removeAll(initialGenerations);
 
       assertThat(createdGenerations).hasSize(1);
@@ -291,23 +174,6 @@ class CreateGenerationInCamundaCloudHandlerTest {
           .containsOnly(
               entry("generation", createdGeneration.name()),
               entry("generationUUID", createdGeneration.uuid()));
-    }
-
-    @Test
-    void shouldDeleteGenerationIfExceptionOccursAfterGenerationHasBennCreated() {
-      // given
-      final var message =
-          "Deliberate runtime exception in shouldDeleteGenerationIfExceptionOccursAfterGenerationHasBennCreated";
-      doThrow(new RuntimeException(message))
-          .when(spyInternalApiClient)
-          .updateChannel(Mockito.any(), Mockito.any());
-
-      // when + then
-      assertThatThrownBy(() -> sutCreateGenerationHandler.handle(jobClientStub, activatedJobStub))
-          .isExactlyInstanceOf(RuntimeException.class)
-          .hasMessage(message);
-
-      verify(spyInternalApiClient).deleteGeneration(Mockito.any());
     }
   }
 
