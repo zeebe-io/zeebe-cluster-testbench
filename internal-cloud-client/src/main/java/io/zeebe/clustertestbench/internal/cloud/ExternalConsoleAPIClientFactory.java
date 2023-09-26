a5f03dc00a52a6c582967052aaee8d163b497242
Christopher Zell <zelldon91@googlemail.com>
2023-09-26 11:45:23 +0200

    feat: add new client for external console API
    Client is created based on https://console.cloud.dev.ultrawombat.com/external/openapi/docs/#/default/CreateClone

---
+++ internal-cloud-client/src/main/java/io/zeebe/clustertestbench/internal/cloud/ExternalConsoleAPIClientFactory.java
@@ -0,0 +1,34 @@
+package io.zeebe.clustertestbench.internal.cloud;
+
+import io.zeebe.clustertestbench.cloud.filter.FailedResponseFilter;
+import io.zeebe.clustertestbench.cloud.oauth.OAuthInterceptor;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+public class ExternalConsoleAPIClientFactory {
+
+  public ExternalConsoleAPIClient createConsoleAPIClient(
+      final String cloudApiUrl,
+      final String authenticationServerURL,
+      final String audience,
+      final String clientId,
+      final String clientSecret,
+      final String username,
+      final String password) {
+
+    final OAuthInterceptor oauthInterceptor =
+        OAuthInterceptor.forUserAccountAuthorization(
+            authenticationServerURL, audience, clientId, clientSecret, username, password);
+
+    final Client client =
+        ClientBuilder.newBuilder()
+            .register(oauthInterceptor)
+            .register(FailedResponseFilter.class)
+            .build();
+    final WebTarget target = client.target(cloudApiUrl);
+    final ResteasyWebTarget rtarget = (ResteasyWebTarget) target;
+    return rtarget.proxy(ExternalConsoleAPIClient.class);
+  }
+}
